5ba505d5216cb1cbeddb79785fa68e238d8fde09
k7hika <kart7hika@outlook.com>
2026-01-16 13:51:26 +0530

    update pdf parser logic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from parsers.csvparser import parse_csv
+# from parsers.pdf_parser import parse_pdf
 data=parse_csv('sample_csvs/Acct_Statement_XXXXXXXX2864_22112025.csv')
+# datapdf=parse_pdf('sample_pdfs/Account_stmt_XX4936_12122025.pdf')
 print(data[0])
+# print(datapdf[0])
